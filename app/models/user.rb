c50b97d5b5abe80bcf847563f2e9dc140cb28c45
Sergey Aksyonov <sergey@artistsoft.com>
2016-06-23 10:36:31 +0300

    Fixed user creation, added secrets.yml

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,13 @@ class User < ActiveRecord::Base
 
   validates_numericality_of :default_category_id, greater_than_or_equal_to: 1, only_integer: true
 
+  before_validation :set_default_category
+
   def default_category
     Category.default_category_for(self)
+  end
+
+  def set_default_category
+    self.default_category_id ||= 1
   end
 end
