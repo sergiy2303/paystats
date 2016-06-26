e7d30b2da31ff6fb59bc7b1c21201a766b009eb0
Sergiy Topchiy <sergiy2303@outlook.com>
2016-06-26 23:29:06 +0300

    Add styles for stats, add default category setup

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,4 +1,9 @@
 class SettingsController < ApplicationController
   def index
   end
+
+  def set_default_category
+    current_user.update(default_category_id: params[:default_category_id])
+    redirect_via_turbolinks_to settings_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :payments
   root 'payments#index'
   resources :stats, only: :index
-  resources :settings, only: :index
+  resources :settings, only: :index do
+    post 'set_default_category', to: 'settings#set_default_category', on: :collection
+  end
   resources :categories
 end
