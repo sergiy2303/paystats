1bd7dc1dd42ace5c4fe0b463ffc1bc77a2899ed7
Serhii Topchii <sergiy2303@outlook.com>
2023-03-02 20:56:36 +0200

    Improve application versions

--- cloud/codedeploy/scripts/install.sh
+++ cloud/codedeploy/scripts/install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+ECR_TAG=608525754125.dkr.ecr.us-east-1.amazonaws.com/paystats:latest
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 608525754125.dkr.ecr.us-east-1.amazonaws.com
-docker pull 608525754125.dkr.ecr.us-east-1.amazonaws.com/paystats:latest
-docker tag 608525754125.dkr.ecr.us-east-1.amazonaws.com/paystats:latest app:latest
+docker pull $ECR_TAG
+docker tag $ECR_TAG app:latest
